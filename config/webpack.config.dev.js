08b510b61778bbd1e2d23e6257d11768fa2cb380
Stephen Koo <stephenyrkoo@gmail.com>
2017-09-18 20:01:27 +1000

    Modify 'webpack.config.dev.js' & 'webpack.config.prod.js' to recognise css-modules

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -166,6 +166,8 @@ module.exports = {
                 loader: require.resolve('css-loader'),
                 options: {
                   importLoaders: 1,
+                  modules: true,
+                  localIdentName: '[name]__[local]___[hash:base64:5]',
                 },
               },
               {
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -176,6 +176,7 @@ module.exports = {
                       loader: require.resolve('css-loader'),
                       options: {
                         importLoaders: 1,
+                        modules: true,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
                       },
